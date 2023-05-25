2957826533b03aeaae8fc1bade11fe67485bd510
Serhemax <stoenkos@gmail.com>
2023-05-25 17:23:05 +0200

    lab15

--- programming-Stoienko/lab15/src/lib.c
+++ programming-Stoienko/lab15/src/lib.c
@@ -1,6 +1,6 @@
 /**
  * @file lib.c
- * @brief 
+ * @brief
  *
  * @author Stoienko Serhii.
  * @date 16-apr-2023
@@ -8,103 +8,100 @@
 
 #include "lib.h"
 
-Bird *read_bird(const char *filename, int *num_birds)
-{
-	FILE *fp;
-	char line[256];
-	Bird *birds;
-	int i = 0;
+Bird *read_bird(const char *filename, int *num_birds) {
+  FILE *fp;
+  char line[256];
+  Bird *birds;
+  int i = 0;
 
-	fp = fopen(filename, "r");
-	if (fp == NULL) {
-		perror("Error opening file");
-		exit(EXIT_FAILURE);
-	}
+  fp = fopen(filename, "r");
+  if (fp == NULL) {
+    perror("Error opening file");
+    exit(EXIT_FAILURE);
+  }
 
-	// Першим рядком в файлі має бути кількість птахів
-	fgets(line, sizeof(line), fp);
-	*num_birds = atoi(line);
-	birds = malloc((size_t)*num_birds * sizeof(Bird));
+  // Першим рядком в файлі має бути кількість птахів
+  fgets(line, sizeof(line), fp);
+  *num_birds = atoi(line);
+  birds = malloc((size_t)*num_birds * sizeof(Bird));
 
-	while (fgets(line, sizeof(line), fp)) {
-		int is_ringed = atoi(strtok(line, ","));
-		char *type = strtok(NULL, ",");
-		int age = atoi(strtok(NULL, ","));
-		int area = atoi(strtok(NULL, ","));
-		int height = atoi(strtok(NULL, ","));
-		int feeders = atoi(strtok(NULL, ","));
-		int has_nest = atoi(strtok(NULL, ","));
-		char *gender = strtok(NULL, " \n");
+  while (fgets(line, sizeof(line), fp)) {
+    int is_ringed = atoi(strtok(line, ","));
+    char *type = strtok(NULL, ",");
+    int age = atoi(strtok(NULL, ","));
+    int area = atoi(strtok(NULL, ","));
+    int height = atoi(strtok(NULL, ","));
+    int feeders = atoi(strtok(NULL, ","));
+    int has_nest = atoi(strtok(NULL, ","));
+    char *gender = strtok(NULL, " \n");
 
-		birds[i].is_ringed = is_ringed;
-		birds[i].type = strdup(type);
-		birds[i].age = age;
-		birds[i].home.area = area;
-		birds[i].home.height = height;
-		birds[i].home.feeders = feeders;
-		birds[i].home.has_nest = has_nest;
-		birds[i].gender = strdup(gender);
-		i++;
-	}
+    birds[i].is_ringed = is_ringed;
+    birds[i].type = strdup(type);
+    birds[i].age = age;
+    birds[i].home.area = area;
+    birds[i].home.height = height;
+    birds[i].home.feeders = feeders;
+    birds[i].home.has_nest = has_nest;
+    birds[i].gender = strdup(gender);
+    i++;
+  }
 
-	fclose(fp);
+  fclose(fp);
 
-	return birds;
+  return birds;
 }
 
 // Функція для запису масиву птахів до файлу
-void write_birds_to_file(const char *filename, const Bird *birds, int num_birds)
-{
-	FILE *file = fopen(filename, "w");
-	if (file == NULL) {
-		printf("Помилка відкриття файлу.\n");
-		return;
-	}
+void write_birds_to_file(const char *filename, const Bird *birds,
+                         int num_birds) {
+  FILE *file = fopen(filename, "w");
+  if (file == NULL) {
+    printf("Помилка відкриття файлу.\n");
+    return;
+  }
 
-	for (int i = 0; i < num_birds; i++) {
-		fprintf(file, "%d %s %d %d %d %d %d %s\n", birds[i].is_ringed,
-			birds[i].type, birds[i].age, birds[i].home.area,
-			birds[i].home.height, birds[i].home.feeders,
-			birds[i].home.has_nest, birds[i].gender);
-	}
+  for (int i = 0; i < num_birds; i++) {
+    fprintf(file, "%d %s %d %d %d %d %d %s\n", birds[i].is_ringed,
+            birds[i].type, birds[i].age, birds[i].home.area,
+            birds[i].home.height, birds[i].home.feeders, birds[i].home.has_nest,
+            birds[i].gender);
+  }
 
-	fclose(file);
+  fclose(file);
 }
 
 // Функція для виведення масиву птахів на екран
-void print_birds(const Bird *birds, int num_birds)
-{
-	for (int i = 0; i < num_birds; i++) {
-		printf("Птах %d:\n", i + 1);
-		printf("  Чи окольцьований: %c\n", birds[i].is_ringed);
-		printf("  Назва виду: %s\n", birds[i].type);
-		printf("  Вік: %d місяців\n", birds[i].age);
-		printf("  Домівка:\n");
-		printf("    Площа: %d кв. см\n", birds[i].home.area);
-		printf("    Висота: %d см\n", birds[i].home.height);
-		printf("    Кількість годівниць: %d\n", birds[i].home.feeders);
-		printf("    Наявність гнізда: %c\n", birds[i].home.has_nest);
-		printf("  Стать: %s\n", birds[i].gender);
-		printf("\n");
-	}
+void print_birds(const Bird *birds, int num_birds) {
+  for (int i = 0; i < num_birds; i++) {
+    printf("Птах %d:\n", i + 1);
+    printf("  Чи окольцьований: %d\n", birds[i].is_ringed);
+    printf("  Назва виду: %s\n", birds[i].type);
+    printf("  Вік: %d місяців\n", birds[i].age);
+    printf("  Домівка:\n");
+    printf("    Площа: %d кв. см\n", birds[i].home.area);
+    printf("    Висота: %d см\n", birds[i].home.height);
+    printf("    Кількість годівниць: %d\n", birds[i].home.feeders);
+    printf("    Наявність гнізда: %d\n", birds[i].home.has_nest);
+    printf("  Стать: %s\n", birds[i].gender);
+    printf("\n");
+  }
 }
 
 // Функція для знаходження відсоткового відношення самок до самців у відділі
-float get_gender_ratio(const Bird *birds, int num_birds)
-{
-	int males = 0;
-	int females = 0;
+float get_gender_ratio(const Bird *birds, int num_birds) {
+  int males = 0;
+  int females = 0;
 
-	for (int i = 0; i < num_birds; i++) {
-		if (strcmp(birds[i].gender, "чоловіча") == 0) {
-			males++;
-		} else if (strcmp(birds[i].gender, "жіноча") == 0) {
-			females++;
-		}
-	}
-	if (males + females > 0) {
-		return (float)females / (float)(males + females) * 100.0f;
-	} else {
-		return 0.0;
-	}
+  for (int i = 0; i < num_birds; i++) {
+    if (strcmp(birds[i].gender, "чоловіча") == 0) {
+      males++;
+    } else if (strcmp(birds[i].gender, "жіноча") == 0) {
+      females++;
+    }
+  }
+  if (males + females > 0) {
+    return (float)females / (float)(males + females) * 100.0f;
+  } else {
+    return 0.0;
+  }
 }
--- programming-Stoienko/lab15/src/main.c
+++ programming-Stoienko/lab15/src/main.c
@@ -14,33 +14,32 @@
 */
 #include "lib.h"
 
-int main(int argc, char *argv[])
-{
-	int num_birds;
-	Bird *birds;
-	int i;
-	if (argc != 3) {
-		fprintf(stderr, "Usage: %s input output\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
+int main(int argc, char *argv[]) {
+  int num_birds;
+  Bird *birds;
+  int i;
+  if (argc != 3) {
+    fprintf(stderr, "Usage: %s input output\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
 
-	birds = read_bird(argv[1], &num_birds);
+  birds = read_bird(argv[1], &num_birds);
 
-	write_birds_to_file(argv[2], birds, num_birds);
-	print_birds(birds, num_birds);
+  write_birds_to_file(argv[2], birds, num_birds);
+  print_birds(birds, num_birds);
 
-	float gender_ratio = get_gender_ratio(birds, num_birds);
+  float gender_ratio = get_gender_ratio(birds, num_birds);
 
-	printf("відсотковe відношення самок до самців у відділі: %.2f%%\n",
-	       gender_ratio);
+  printf("відсотковe відношення самок до самців у відділі: %.2f%%\n",
+         gender_ratio);
 
-	// Звільняємо пам'ять, яку було виділено для зберігання даних про птахів
-	for (i = 0; i < num_birds; i++) {
-		free((birds + i)->type);
-		free((birds + i)->gender);
-	}
+  // Звільняємо пам'ять, яку було виділено для зберігання даних про птахів
+  for (i = 0; i < num_birds; i++) {
+    free((birds + i)->type);
+    free((birds + i)->gender);
+  }
 
-	free(birds);
+  free(birds);
 
-	return 0;
+  return 0;
 }
